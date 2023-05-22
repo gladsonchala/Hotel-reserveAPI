764276bd6c2e1371299256b1e73b024e6065a0cf
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-22 18:58:38 +0300

    Update errorHandler.js

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,20 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ error: 'Internal server error' });
+
+  // Set default status code and error message
+  let statusCode = 500;
+  let errorMessage = 'Internal Server Error';
+
+  // Handle specific error types
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    errorMessage = err.message;
+  } else if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    statusCode = 400;
+    errorMessage = 'Invalid ID';
+  }
+
+  res.status(statusCode).json({ error: errorMessage });
 };
 
 module.exports = errorHandler;
