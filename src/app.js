d7270060ba0b7f4ebb7aaede941bef70cc74073b
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-22 18:57:51 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const hotelRoutes = require('./routes/hotelRoutes');
+const roomRoutes = require('./routes/roomRoutes');
+const apiKeyRoutes = require('./routes/apiKeyRoutes');
+const errorHandler = require('./utils/errorHandler');
+
 dotenv.config();
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+const app = express();
+
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(morgan('dev'));
 
 // Routes
-app.use('/api', require('./routes/hotelRoutes'));
-app.use('/api', require('./routes/roomRoutes'));
-app.use('/api', require('./routes/apiKeyRoutes'));
+app.use('/hotels', hotelRoutes);
+app.use('/rooms', roomRoutes);
+app.use('/apikeys', apiKeyRoutes);
 
-// Error handler middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: 'Server error' });
-});
+// Error handling middleware
+app.use(errorHandler);
 
 // Start the server
 const port = process.env.PORT || 3000;
