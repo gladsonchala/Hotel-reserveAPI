1f99e26e931d385c290c32e6fa3e0fa94b982127
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-22 15:40:05 +0300

    Create roomRoutes.js

---
+++ src/routes/roomRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const roomController = require('../controllers/roomController');
+const { authenticate, authorize } = require('../middleware/authenticationMiddleware');
+
+router.get('/', authenticate, roomController.getAllRooms);
+router.get('/:room_id', authenticate, roomController.getRoomById);
+router.post('/', authenticate, authorize('admin'), roomController.createRoom);
+router.put('/:room_id', authenticate, authorize('admin'), roomController.updateRoom);
+router.delete('/:room_id', authenticate, authorize('admin'), roomController.deleteRoom);
+
+module.exports = router;
