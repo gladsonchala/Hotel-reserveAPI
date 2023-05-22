f04a149047526d03682ab6a76afa3b128fa79015
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-22 18:59:59 +0300

    Update apiKeyRoutes.js

--- src/routes/apiKeyRoutes.js
+++ src/routes/apiKeyRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const apiKeyController = require('../controllers/apiKeyController');
-const { authenticate } = require('../middleware/authenticationMiddleware');
+const { authenticate, authorize } = require('../middleware/authenticationMiddleware');
 
-router.get('/', authenticate, apiKeyController.getAllApiKeys);
-router.get('/:key_id', authenticate, apiKeyController.getApiKeyById);
-router.post('/', authenticate, apiKeyController.createApiKey);
-router.delete('/:key_id', authenticate, apiKeyController.deleteApiKey);
+router.get('/', authenticate, authorize('admin'), apiKeyController.getAllApiKeys);
+router.get('/:key_id', authenticate, authorize('admin'), apiKeyController.getApiKeyById);
+router.post('/', authenticate, authorize('admin'), apiKeyController.createApiKey);
+router.delete('/:key_id', authenticate, authorize('admin'), apiKeyController.deleteApiKey);
 
 module.exports = router;
